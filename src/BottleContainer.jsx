94b7561f90a1dde516c1ed6b8857a710949e3fea
chris <you@example.com>
2024-05-19 16:18:39 +0300

    wrapping in gamescreen 2

--- src/BottleContainer.jsx
+++ src/BottleContainer.jsx
@@ -2,69 +2,11 @@ import { useState, useEffect, useRef } from "react";
 import Bottle from "./Bottle";
 import * as classes from "./BottleContainer.module.css";
 
-function BottleContainer() {
-  const [BOTTLE_CAPACITY, NUM_BOTTLES, NUM_EMPTY_BOTTLES] = [4, 4, 2];
+function BottleContainer(props) {
+  const BOTTLE_CAPACITY = props.bottleCapacity;
   const [height, setHeight] = useState(0);
   const ref = useRef(null);
-  const [bottleArray, setBottleArray] = useState(initializeBottleArray());
   const [selectedBottles, setSelectedBottle] = useState([-1, -1]);
-
-  function initializeBottleArray() {
-    const allBottles = [];
-    const totalLiquids = createTotalLiquids();
-
-    for (let i = 0; i < NUM_BOTTLES; i++) {
-      const bottle = [];
-      for (let j = 0; j < BOTTLE_CAPACITY; j++) {
-        
-        let randomIndex = randomNumberBetween(0, totalLiquids.length - 1);
-        let randomNum = totalLiquids[randomIndex];
-          // make sure that we cannot begin with already sorted bottles
-        if (j === BOTTLE_CAPACITY - 1) {
-          while (areAllElementsSame(bottle)&&bottle[0]===randomNum) {
-            randomIndex = randomNumberBetween(0, totalLiquids.length - 1);
-            randomNum = totalLiquids[randomIndex];
-          }
-        }
-
-        totalLiquids.splice(randomIndex, 1);
-        bottle.push(randomNum);
-      }
-      allBottles.push(bottle);
-    }
-
-    // adds empty bottles
-    for (let i = 0; i < NUM_EMPTY_BOTTLES; i++) {
-      const emptyBottle = [];
-      allBottles.push(emptyBottle);
-    }
-    return allBottles;
-
-    function randomNumberBetween(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    function areAllElementsSame(arr) {
-      if (arr.length === 0) {
-          return true;
-      }
-      for (let i = 1; i < arr.length; i++) {
-          if (arr[i] !== arr[0]) {
-              return false;
-          }
-      }
-      
-      return true;
-    }
-    function createTotalLiquids() {
-      const arr = [];
-      for (let i = 0; i < BOTTLE_CAPACITY; i++) {
-        for (let j = 1; j < NUM_BOTTLES + 1; j++) {
-          arr.push(j);
-        }
-      }
-      return arr;
-    }
-  }
 
   function handleClick(key) {
     if (selectedBottles[0] === -1) {
@@ -75,7 +17,7 @@ function BottleContainer() {
       return;
     } else {
       // click the second bottle
-      if (bottleArray[key].length >= BOTTLE_CAPACITY) {
+      if (props.bottleArray[key].length >= BOTTLE_CAPACITY) {
         // target is full
         return;
       } else {
@@ -95,7 +37,7 @@ function BottleContainer() {
 
   // pours liquids from one bottle to another
   const pourLiquidsToTargetBottle = () => {
-    const newState = [...bottleArray]; // Make a shallow copy of the state array
+    const newState = [...props.bottleArray]; // Make a shallow copy of the state array
     const arrayOrigin = newState[selectedBottles[0]];
     const arrayDestination = newState[selectedBottles[1]];
     // you cannot pour on a different color
@@ -103,21 +45,20 @@ function BottleContainer() {
       return;
     }
     const freeSlots = arrayDestination - BOTTLE_CAPACITY;
-
     const originElement = arrayOrigin.shift(); // Remove the first element from the first subarray
     const topElement = originElement;
     arrayDestination.unshift(topElement); // Push the removed element into the third subarray
-    setBottleArray(newState);
+    props.setBottleArray(newState);
   };
 
   // sets the height of each drop of liquid in the bottles
   useEffect(() => {
-    setHeight(Math.floor(ref.current.clientHeight / (BOTTLE_CAPACITY + 1)));
+    setHeight(Math.floor(ref.current.clientHeight / (BOTTLE_CAPACITY *1.05)));
   });
 
   return (
     <div className={classes.container}>
-      {bottleArray.map((_, index) => (
+      {props.bottleArray.map((_, index) => (
         <div
           ref={ref}
           key={index}
@@ -125,7 +66,7 @@ function BottleContainer() {
           className={`${classes.bottle} 
           ${index === selectedBottles[0] || index === selectedBottles[1] ? classes.selected : ""}`}
         >
-          <Bottle contents={bottleArray[index]} height={height} />
+          <Bottle contents={props.bottleArray[index]} height={height} />
         </div>
       ))}
     </div>
--- src/GameScreen.jsx
+++ src/GameScreen.jsx
@@ -3,12 +3,75 @@ import BottleContainer from "./BottleContainer";
 import * as classes from "./GameScreen.module.css";
 
 function GameScreen() {
+    const [BOTTLE_CAPACITY, NUM_BOTTLES, NUM_EMPTY_BOTTLES] = [4, 4, 2];
+    const [bottleArray, setBottleArray] = useState(initializeBottleArray());
 
 
-
-    function handleNew() {
-        
+    function initializeBottleArray() {
+        const allBottles = [];
+        const totalLiquids = createTotalLiquids();
+    
+        for (let i = 0; i < NUM_BOTTLES; i++) {
+          const bottle = [];
+          for (let j = 0; j < BOTTLE_CAPACITY; j++) {
+            
+            let randomIndex = randomNumberBetween(0, totalLiquids.length - 1);
+            let randomNum = totalLiquids[randomIndex];
+              // make sure that we cannot begin with already sorted bottles
+            if (j === BOTTLE_CAPACITY - 1) {
+              while (areAllElementsSame(bottle)&&bottle[0]===randomNum) {
+                randomIndex = randomNumberBetween(0, totalLiquids.length - 1);
+                randomNum = totalLiquids[randomIndex];
+              }
+            }
+    
+            totalLiquids.splice(randomIndex, 1);
+            bottle.push(randomNum);
+          }
+          allBottles.push(bottle);
+        }
+    
+        // adds empty bottles
+        for (let i = 0; i < NUM_EMPTY_BOTTLES; i++) {
+          const emptyBottle = [];
+          allBottles.push(emptyBottle);
+        }
+        return allBottles;
+    
+        function randomNumberBetween(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        function areAllElementsSame(arr) {
+          if (arr.length === 0) {
+              return true;
+          }
+          for (let i = 1; i < arr.length; i++) {
+              if (arr[i] !== arr[0]) {
+                  return false;
+              }
+          }
+          
+          return true;
+        }
+        function createTotalLiquids() {
+          const arr = [];
+          for (let i = 0; i < BOTTLE_CAPACITY; i++) {
+            for (let j = 1; j < NUM_BOTTLES + 1; j++) {
+              arr.push(j);
+            }
+          }
+          return arr;
+        }
     }
+    
+
+    
+    function handleNew() {
+        console.log(bottleArray)
+        setBottleArray(b=>[...bottleArray,[0,0,0,4]])
+        console.log(bottleArray)
+    }
+
     function handleReset() {
         
     }
@@ -17,7 +80,7 @@ function GameScreen() {
     }
   return (
     <div>
-      <BottleContainer />
+      <BottleContainer bottleArray={bottleArray} setBottleArray={ setBottleArray} bottleCapacity={BOTTLE_CAPACITY } />
       <button onClick={()=>handleNew()}>New Level</button>
       <button onClick={()=>handleReset()}>Reset</button>
       <button onClick={()=>handleUndo()}>Undo</button>
